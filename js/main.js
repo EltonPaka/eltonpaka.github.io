20bf1f499c457f73c8ce4955882242388db367ea
Elton Paka <eltpaka@amazon.com>
2019-07-04 16:07:59 +0200

    test1

--- js/main.js
+++ js/main.js
@@ -1,57 +1,15 @@
+var url = 'https://app.auth.eu-west-1.amazoncognito.com/login'; 
+var params = "redirect_uri=https://www.google.com&response_type=token&client_id=6a5t8g79dfo5tffpjcjmsuvj1k"
 
-(function ($) {
-    "use strict";
+function load(url, callback) {
+  var xhr = new XMLHttpRequest();
 
-
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      callback(xhr.response);
     }
+  }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
+  xhr.open('GET', url+'?'+params);
+  xhr.send('');
+}
